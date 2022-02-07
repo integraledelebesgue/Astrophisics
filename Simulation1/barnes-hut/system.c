40ff6d40164b8016c0959c35d8bd53101a145a2c
integraledelebesgue <jsmolka@student.agh.edu.pl>
2022-02-07 16:39:56 +0100

    structures

--- Simulation1/barnes-hut/system.c
+++ Simulation1/barnes-hut/system.c
@@ -1,4 +1,39 @@
-//
-// Created by jsmolka on 05.02.2022.
-//
+#include <math.h>
+#include<stdbool.h>
 
+struct system {
+    int *bodies;
+    int mass_scale;
+    int radius_scale;
+};
+
+struct point {
+    double x;
+    double y;
+};
+
+struct velocity {
+    double x_direction;
+    double y_direction;
+};
+
+struct body {
+    double mass;  // mantises only
+    double radius;
+    struct point position;
+    struct velocity velocity;
+};
+
+struct node {
+    int data;
+    double size;
+    struct point centre;
+    bool divided;
+
+    struct body* bodies;
+
+    struct node* NW;
+    struct node* NE;
+    struct node* SW;
+    struct node* SE;
+};
