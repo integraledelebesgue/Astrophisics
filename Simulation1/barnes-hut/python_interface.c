24fd93475ad7b239989945593353d19c4d921a2d
integraledelebesgue <jsmolka@student.agh.edu.pl>
2022-03-19 23:19:08 +0100

    Compiled C prototype, Python usage attempt.

--- Simulation1/barnes-hut/python_interface.c
+++ Simulation1/barnes-hut/python_interface.c
@@ -1,3 +1,17 @@
 #include<Python.h>
-#include <stddef.h>
+#include<stddef.h>
+#include<malloc.h>
 #include "headers.h"
+
+static PyObject* computeForces(PyObject *self, PyObject *args){
+    int count;
+    double **state = (double **)malloc(count*sizeof(double));
+    double accuracy;
+
+    if(!PyArg_ParseTuple(args, "y*id", state, &count, &accuracy)){
+        return NULL;
+    }
+
+    double **forces = perform(count, state, accuracy);
+}
+
