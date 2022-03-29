ddbca874b2bcfc7f20ec76facccca1a4faa1a413
integraledelebesgue <jsmolka@student.agh.edu.pl>
2022-03-29 14:42:38 +0200

    Git mess fix

--- Simulation1/Elements/Quadtree.cpp
+++ Simulation1/Elements/Quadtree.cpp
@@ -19,11 +19,6 @@ void constructTree(Node &root){
         //cout << "In loop..";
         curr = Stack.top();
 
-        if(curr->radius == 0){
-            printf("!!! Radius 0 !!!");
-            exit(0);
-        }
-
         qualifyBodies(curr, NW, NE, SW, SE);
         Stack.pop();
 
@@ -89,27 +84,9 @@ void qualifyBodies(Node *curr, list<Body> &NW, list<Body> &NE, list<Body> &SW, l
     for(const auto &body : curr->bodies){
         position = body.position - curr->center;
 
-        if(position.x > 0 && position.y < 0) NW.emplace_back(body);
-        else if(position.x <= 0 && position.y <= 0) NE.emplace_back(body);
-        else if(position.x >= 0 && position.y >= 0) SW.emplace_back(body);
-        else if(position.x < 0 && position.y > 0) SE.emplace_back(body);
-
+        if(position.x < 0 && position.y > 0) NW.emplace_back(body);
+        else if(position.x >= 0 && position.y >= 0) NE.emplace_back(body);
+        else if(position.x <= 0 && position.y <= 0) SW.emplace_back(body);
+        else if(position.x > 0 && position.y < 0) SE.emplace_back(body);
     }
-
-    cout << " $$ Qualified nodes: $$" << endl;
-    printf("NW: {");
-    for(auto const &body : NW) printf("[%lf, %lf, %lf], ", body.mass, body.position.x, body.position.y);
-    printf("}\n\n");
-
-    printf("NE: {");
-    for(auto const &body : NE) printf("[%lf, %lf, %lf], ", body.mass, body.position.x, body.position.y);
-    printf("}\n\n");
-
-    printf("SW: {");
-    for(auto const &body : SW) printf("[%lf, %lf, %lf], ", body.mass, body.position.x, body.position.y);
-    printf("}\n\n");
-
-    printf("SE: {");
-    for(auto const &body : SE) printf("[%lf, %lf, %lf], ", body.mass, body.position.x, body.position.y);
-    printf("}\n\n");
 }
--- Simulation1/Elements/cmake-build-debug/CMakeFiles/Elements.dir/Vector.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/Elements.dir/Vector.cpp.o: \
- /home/jsmolka/Programowanie/Projekty/Astrophisics/Simulation1/Elements/Vector.cpp \
- /usr/include/stdc-predef.h \
- /home/jsmolka/Programowanie/Projekty/Astrophisics/Simulation1/Elements/Vector.h
