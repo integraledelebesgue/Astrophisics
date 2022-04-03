94bf663af57df68cb69683294e125255b5d9a9bd
integraledelebesgue <Ricci0curv@ture>
2022-04-03 19:22:24 +0200

    IPython part base

--- Simulation1/cpp_builder/build.sh
+++ Simulation1/cpp_builder/build.sh
@@ -1,4 +1,9 @@
 #!bin/bash
 echo "Constructing C++ shared library.."
 cd ../Elements
-g++ -shared -fPIC Vector.h Body.h Node.h Quadtree.h Physics.h barnes-hut.h memory.h Vector.cpp Body.cpp Node.cpp Quadtree.cpp Physics.cpp barnes-hut.cpp memory.cpp -o barneshut.so
+
+g++ -O3 -Wall -shared -fPIC $(python3 -m pybind11 --includes) Vector.h Body.h Node.h Quadtree.h Physics.h barnes-hut.h memory.h Vector.cpp Body.cpp Node.cpp Quadtree.cpp Physics.cpp barnes-hut.cpp memory.cpp python_interface.cpp -o barneshut.so
+
+cp -v ./barneshut.so ../
+
+echo "Finished!"
