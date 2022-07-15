4428f6f18800007c9eae522e7ed12c5b5c6ca172
integraledelebesgue <Ricci0curv@ture>
2022-07-15 13:04:43 +0200

    Consistency

--- Simulation1/Elements/barnes-hut.cpp
+++ Simulation1/Elements/barnes-hut.cpp
@@ -12,50 +12,26 @@
 #include<chrono>
 
 using namespace std;
-//double threshold;
-//std::stack<void *> *memStack;
 
 vector<vector<double>> getForces(const vector<vector<double>> &state, int count, double radius, double accuracy){
     Vector *result;
     list<Body> bodies;
     threshold = accuracy;
 
-    //cout << "Computation starting.. " << endl;
-
-    //printArr(state, count);
-
     auto start = chrono::high_resolution_clock::now();
 
     result = new Vector[count];
     memStack = new std::stack<void *>;
 
-    /*double **result_arr = new double *[count];
-
-    for(int i=0; i<count; i++){
-        result_arr[i] = new double[2];
-    }*/
-
     vector<vector<double>> result_arr(count);
-
-    //cout << "Conversion.. " << endl;
 
     stateToBody(state, bodies, count);
 
     Node root(radius, Vector(), bodies);
 
-    //cout << "Tree construction.." << endl;
-
     constructTree(root);
-
-    //cout << "Force computation.. " << endl;
-
     computeForces(root, result);
-
-    //cout << "Conversion.. " << endl;
-
     vectorToArr(result_arr, result, count);
-
-    //cout << "Done!" << endl;
 
     auto stop = chrono::high_resolution_clock::now();
     auto duration = chrono::duration_cast<chrono::microseconds>(stop - start);
@@ -71,16 +47,7 @@ vector<vector<double>> getForces(const vector<vector<double>> &state, int count,
     log.close();
 
     delete[](result);
-
-    //root.traverse(printNode);
-
-    //cout << "Cleaning up memory.. ";
-
     freeMemory();
-
-    //cout << "Memory stack: ";
-    //cout << memStack->size() << endl;
-
     delete(memStack);
 
     return result_arr;
